def15c393e5d36167581ad0624d2425000e453c9
R0man0s <rkotelevits@gmail.com>
2021-10-26 23:02:47 +0300

    Lecture 25 was done

--- src/section11/Main.java
+++ src/section11/Main.java
@@ -1,8 +1,29 @@
 package section11;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("hello");
+        words.add("there");
+        words.add("10");
+
+        String item1 = words.get(2);
+
+        System.out.println(item1);
+
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(45);
+        numbers.add(1000);
+        numbers.removeFirst();
+
+        for (int number: numbers) {
+            System.out.println(number);
+        }
     }
 }
