f9bfdac3901857807ed9217ef13db2ae7469b331
R0man0s <rkotelevits@gmail.com>
2021-10-28 14:16:57 +0300

    Section 15, Lesson #35 was done

---
+++ src/s15_L35/threading/Application.java
@@ -0,0 +1,25 @@
+package s15_L35.threading;
+
+public class Application {
+    public static void main(String[] args) {
+        Task taskRunner = new Task();
+        taskRunner.start();
+
+        System.out.println("hello there..");
+
+        Task taskRunner2 = new Task();
+        taskRunner2.start();
+
+
+    }
+}
+
+class Task extends Thread {
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("number: " + i);
+
+        }
+    }
+}
