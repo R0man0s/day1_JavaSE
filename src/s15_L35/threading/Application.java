08d2a0ab212ff0b9e87cfc148ac93ed3aef2b547
R0man0s <rkotelevits@gmail.com>
2021-10-28 15:44:29 +0300

    Section 15, Lesson #36 was done

--- src/s15_L35/threading/Application.java
+++ src/s15_L35/threading/Application.java
@@ -2,23 +2,94 @@ package s15_L35.threading;
 
 public class Application {
     public static void main(String[] args) {
-        Task taskRunner = new Task();
-        taskRunner.start();
 
-        System.out.println("hello there..");
+        System.out.println("Starting Tread 1");
+        //Task taskRunner = new Task("Tread - A");
+        //taskRunner.start();
+        //taskRunner.run();
+//        Thread t1 = new Thread(new Task("Tread - A"));
+//        t1.start();
 
-        Task taskRunner2 = new Task();
-        taskRunner2.start();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t1.start();
 
+
+        System.out.println("Starting Tread 2");
+        //Task taskRunner2 = new Task("Tread - B");
+        //taskRunner2.start();
+        //taskRunner2.run();
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t2.start();
 
     }
 }
 
-class Task extends Thread {
+//class Task extends Thread {
+//    String name;
+//
+//    public Task(String name) {
+//        this.name = name;
+//    }
+//
+//    public void run() {
+//
+//        Thread.currentThread().setName(name);
+//
+//        for (int i = 0; i < 500; i++) {
+//            System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
+//}
+
+class Task implements Runnable {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
 
     public void run() {
-        for (int i = 0; i < 1000; i++) {
-            System.out.println("number: " + i);
+
+        Thread.currentThread().setName(name);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
